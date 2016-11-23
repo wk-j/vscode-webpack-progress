92dd23b2b075db01fc3c06af9ee103e7592bdcdd
Saleh Abdel Motaal <saleh@grasppe.com>
2016-11-23 13:33:30 -0500

    Implemented very basic "status-percent-timeout-when-complete" feature with change to WebpackProgress class in "/src/webpack.ts". Now in the updateProgress method, if the percentage is 100 and the lastPercentage was not undefined, it calls the resetProgress which reset the status bar text to "Webpack √" then after a timeout to "Webpack". Also, instead of manually prepending "Webpack" to the status bar text, it now prepends this.statusLabel which defaults to "Webpack", for consistency/soc.
    There are no changes to any other files, tests, scripts or dependencies.

--- src/webpack.ts
+++ src/webpack.ts
@@ -19,7 +19,7 @@ export class ProgressWatcher extends EventEmitter {
             });
         }, 1000);
     }
-    
+
     dispose() {
         clearInterval(this.timer);
     }
@@ -27,16 +27,28 @@ export class ProgressWatcher extends EventEmitter {
 
 export class WebpackProgress {
      private statusBarItem: vscode.StatusBarItem;
-     
+     private lastPercentage: number;
+     private resetTimout: any;
+     private statusLabel:string = 'Webpack';
+
      updateProgress(percentage) {
          if(!this.statusBarItem) {
              this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right);
-             this.statusBarItem.text = "Webpack 0%";
+             this.statusBarItem.text = this.statusLabel;
              this.statusBarItem.show();
          }
-         this.statusBarItem.text = `Webpack ${percentage}%`;
+        if (typeof this.lastPercentage === 'undefined') this.statusBarItem.text = this.statusLabel;
+        else this.statusBarItem.text = typeof percentage == 'number' ? `${this.statusLabel} ${percentage}%` : this.statusLabel;
+        if (percentage == 100 && typeof this.lastPercentage !== 'undefined') this.resetProgress();
+        this.lastPercentage = percentage;
      }
-     
+
+    resetProgress() {
+        this.statusBarItem.text = `${this.statusLabel} √`;
+        clearTimeout(this.resetTimout);
+        this.resetTimout = setTimeout(() => this.statusBarItem.text = this.statusLabel, 5000);
+    }
+
      dispose() {
          this.statusBarItem.dispose();
      }
